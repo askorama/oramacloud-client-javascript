47c92183eb00459d2672df908427cf3ecd144aad
Tommaso Allevi <tomallevi@gmail.com>
2024-01-11 16:43:34 +0100

    Encrypt messages

--- src/proxy.ts
+++ src/proxy.ts
@@ -40,6 +40,7 @@ export class OramaProxy {
   private CSRFToken = ''
   private ready: Promise<boolean>
   private readonly api_key: string
+  private publicKey?: CryptoKey
 
   constructor(params: IOramaProxy) {
     this.api_key = params.api_key
@@ -94,11 +95,27 @@ export class OramaProxy {
   public async *chatStream(params: ChatParams): AsyncGenerator<string> {
     const endpoint = `${CONST.ORAMA_PROXY_ENDPOINT}${CONST.ORAMA_PROXY_CHAT_ENDPOINT}?apiKey=${this.api_key}`
 
+    let messages = params.messages
+    if (this.publicKey) {
+      messages = await Promise.all(messages.map(async (message) => {
+        if (!message.content) {
+          return message
+        }
+        if (typeof message.content !== 'string') {
+          return message
+        }
+        return {
+          ...message,
+          content: await encryptData(this.publicKey!, message.content)
+        }
+      })) as any // Too hard to type this
+    }
+
     const response = await fetch(endpoint, {
       method: 'POST',
       headers: this.getHeaders(),
       body: new URLSearchParams({
-        messages: JSON.stringify(params.messages),
+        messages: JSON.stringify(messages),
         csrf: this.CSRFToken,
         model: chatModels[params.model]
       }).toString()
@@ -132,6 +149,9 @@ export class OramaProxy {
 
     const data = await response.json()
     this.CSRFToken = data.csrfToken
+    if (data.encryption?.enabled === true) {
+      this.publicKey = await importPublicKey(restoreKey(data.encryption.publicKey))
+    }
   }
 
   private isServer() {
@@ -148,4 +168,42 @@ export class OramaProxy {
   private getReferrer() {
     return this.isServer() ? 'http://localhost' : window.location.href
   }
+}
+
+async function importPublicKey(keyData: ArrayBuffer) {
+  const key = await crypto.subtle.importKey(
+    "spki",
+    keyData,
+    { name: "RSA-OAEP", hash: { name: "SHA-256" } },
+    true,
+    ["encrypt"]
+  );
+  return key;
+}
+
+async function encryptPieceData(publicKey: CryptoKey, encodedData: Uint8Array) {
+  const encryptedData = await crypto.subtle.encrypt(
+      { name: "RSA-OAEP" },
+      publicKey,
+      encodedData
+  );
+  return new Uint8Array(encryptedData);
+}
+
+function restoreKey(s: number[]): Uint8Array {
+  return Uint8Array.from(s)
+}
+
+const BATCH_SIZE = 50
+async function encryptData(publicKey: CryptoKey, data: string) {
+  const encodedData = new TextEncoder().encode(data);
+
+  const arr = []
+  for (let i = 0; i < encodedData.length; i += BATCH_SIZE) {
+    const item = encodedData.subarray(i, i + BATCH_SIZE)
+    const encryptedData = await encryptPieceData(publicKey, item)
+    arr.push(Array.from(encryptedData))
+  }
+
+  return arr
 }
