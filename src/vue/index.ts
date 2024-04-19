6ae22c794cf08fa7d2ae939c3bcf922ad6609f4d
SaraVieira <hey@iamsaravieira.com>
2024-04-19 10:31:24 +0100

    format

--- src/vue/index.ts
+++ src/vue/index.ts
@@ -1,37 +1,37 @@
-import type { Results, Nullable, AnyDocument } from "@orama/orama";
-import { OramaClient, type ClientSearchParams } from "../client.js";
-import { Ref, onMounted, ref } from "vue";
+import type { Results, Nullable, AnyDocument } from '@orama/orama'
+import { OramaClient, type ClientSearchParams } from '../client.js'
+import { Ref, onMounted, ref } from 'vue'
 
 interface UseSearch {
-  ready: Ref<boolean>;
-  results: Ref<Nullable<Results<AnyDocument>>>;
-  error: Ref<Nullable<Error>>;
+  ready: Ref<boolean>
+  results: Ref<Nullable<Results<AnyDocument>>>
+  error: Ref<Nullable<Error>>
 }
 
-type useSearchParams = ClientSearchParams & { client: Omit<OramaClient, "id"> };
+type useSearchParams = ClientSearchParams & { client: Omit<OramaClient, 'id'> }
 
 export function useSearch(query: useSearchParams): UseSearch {
-  const ready = ref(false);
-  const results: Ref<Nullable<Results<AnyDocument>>> = ref(null);
-  const error: Ref<Nullable<Error>> = ref(null);
+  const ready = ref(false)
+  const results: Ref<Nullable<Results<AnyDocument>>> = ref(null)
+  const error: Ref<Nullable<Error>> = ref(null)
 
   onMounted(async () => {
     if (!query.client) {
-      throw new Error("No client was passed");
+      throw new Error('No client was passed')
     }
-    ready.value = true;
+    ready.value = true
 
     try {
-      const oramaResults = await query.client?.search(query);
-      results.value = oramaResults;
+      const oramaResults = await query.client?.search(query)
+      results.value = oramaResults
     } catch (e: any) {
-      error.value = e;
+      error.value = e
     }
-  });
+  })
 
   return {
     ready,
     results,
-    error,
-  };
+    error
+  }
 }
